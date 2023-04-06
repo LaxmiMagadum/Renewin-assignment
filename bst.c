81ba60579702daf94dee58fbfec40947333b4bc0
LaxmiMagadum <magadumlaxmi123@gmail.com>
2023-04-06 16:51:34 +0530

    All errors are corrected and code is completed

--- bst.c
+++ bst.c
@@ -1,173 +1,221 @@
-// Name : Laxmi magadum
-// USN : 4SU19CS045
+//Name : Laxmi Magadum
+//USN : 4SU19CS045
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define println(...) printf(__VA_ARGS__);printf("\n");
-
-// Struct to represent a file or directory node
-struct file_node {
+#define println(...)                                                           \
+  printf(__VA_ARGS__);                                                         \
+  printf("\n");
+// Define a struct for a file or directory
+typedef struct {
   char *name;
-  bool is_directory;
-  struct file_node *left_child;
-  struct file_node *right_child;
-};
+  int is_dir;
+} NodeData;
 
-// Root node of the directory structure BST
-struct file_node *root = NULL;
+// Define a node in the binary search tree
+typedef struct Node {
+  NodeData *data;
+  struct Node *left;
+  struct Node *right;
+} Node;
 
-// Function to create a new file or directory node
-struct file_node *create_node(char *name, bool is_directory) {
-  struct file_node *node = (struct file_node *)malloc(sizeof(struct file_node));
-  node->name = name;
-  node->is_directory = is_directory;
-  node->left_child = NULL;
-  node->right_child = NULL;
+// Define a binary search tree
+typedef struct {
+  Node *root;
+} BST;
+
+BST bst = {.root = NULL};
+// Create a new node with the given data
+Node *create_node(NodeData *data) {
+  Node *node = (Node *)malloc(sizeof(Node));
+  node->data = (NodeData*)malloc(sizeof(NodeData));
+  node->data->name = (char*)malloc(strlen(data->name) + 1);
+  for(int i=0;i<strlen(data->name);i++){
+    node->data->name[i] = data->name[i];
+  }
+  node->left = NULL;
+  node->right = NULL;
   return node;
 }
 
-// Function to insert a new file or directory node
-void insert_node(char *name, bool is_directory) {
-  struct file_node *node = create_node(name, is_directory);
-  if (root == NULL) {
-    println("Creating root node");
-    root = node;
+// Insert a node into the binary search tree
+void insert(BST *bst, NodeData *data) {
+  if (bst->root == NULL) {
+    bst->root = create_node(data);
   } else {
-    println("Creating non-root node")
-    struct file_node *temp_node = root;
-    while (true) {
-      if (strcmp(name, temp_node->name) < 0) {
-        if (temp_node->left_child == NULL) {
-          temp_node->left_child = node;
+    Node *cur_node = bst->root;
+    while (cur_node) {
+      if (strcmp(data->name, cur_node->data->name) < 0) {
+        if (cur_node->left == NULL) {
+          cur_node->left = create_node(data);
           break;
         }
-        temp_node = temp_node->left_child;
+        cur_node = cur_node->left;
       } else {
-        if (temp_node->right_child == NULL) {
-          temp_node->right_child = node;
+        if (cur_node->right == NULL) {
+          cur_node->right = create_node(data);
           break;
         }
-        temp_node = temp_node->right_child;
+        cur_node = cur_node->right;
       }
     }
   }
-}// the function sets the empty child node to the new node, completing the insertion process.
-
-// Function to find the minimum node in the right subtree of a node to be
-// deleted
-struct file_node *find_min_node(struct file_node *node) {
-  struct file_node *temp_node = node;
-  while (temp_node->left_child != NULL) {
-    temp_node = temp_node->left_child;
-  }
-  return temp_node;
 }
 
-// Function to delete an existing file or directory node
-struct file_node *delete_node(struct file_node *node, char *name) {
+Node *search_bst(Node *node, char *name) {
   if (node == NULL) {
-    return node;
+    return NULL;
   }
-  if (strcmp(name, node->name) < 0) {
-    node->left_child = delete_node(node->left_child, name);
-  } else if (strcmp(name, node->name) > 0) {
-    node->right_child = delete_node(node->right_child, name);
+  if (strcmp(node->data->name, name) == 0) {
+    return node;
+  } else if (strcmp(name, node->data->name) < 0) {
+    return search_bst(node->left, name);
   } else {
-    if (node->left_child == NULL) {
-      struct file_node *temp_node = node->right_child;
-      free(node);
-      return temp_node;
-    } else if (node->right_child == NULL) {
-      struct file_node *temp_node = node->left_child;
-      free(node);
-      return temp_node;
+    return search_bst(node->right, name);
+  }
+  return NULL;
+}
+// Print the binary search tree in alphabetical order
+void print_bst_inorder(Node *node, int level) {
+  if (node) {
+    print_bst_inorder(node->left, level + 4);
+    for (int i = 0; i < level; i++) {
+      printf(" ");
     }
-    struct file_node *temp_node = find_min_node(node->right_child);
-    node->name = temp_node->name;
-    node->is_directory = temp_node->is_directory;
-    node->right_child = delete_node(node->right_child, temp_node->name);
-  }
-  return node;//The function returns a pointer to the root node of the modified binary search tree.
-}
-
-// Function to search for a file or directory node by name
-struct file_node *search_node(struct file_node *node, char *name) {
-  if (node == NULL || strcmp(name, node->name) == 0) {
-    return node;
-  }
-  if (strcmp(name, node->name) < 0) {
-    return search_node(node->left_child, name);
-  }
-  return search_node(node->right_child, name);
-}
-
-// Function to traverse the directory structure in pre-order
-void traverse_preorder(struct file_node *node) {
-  if (node != NULL) {
-    println("Traversing in Preorder");
-    printf("%s\n", node->name);
-    traverse_preorder(node->left_child);
-    traverse_preorder(node->right_child);
+    if (node->data->is_dir) {
+      printf("%s/\n", node->data->name);
+    } else {
+      printf("%s\n", node->data->name);
+    }
+    print_bst_inorder(node->right, level + 4);
   }
 }
-
-// Function to traverse the directory structure in in-order
-void traverse_inorder(struct file_node *node) {
-  if (node != NULL) {
-    println("Traversing in Inorder");
-    traverse_inorder(node->left_child);
-    printf("%s\n", node->name);
-    traverse_inorder(node->right_child);
+void print_bst_postorder(Node *node, int level) {
+  if (node) {
+    print_bst_postorder(node->right, level + 4);
+    print_bst_postorder(node->left, level + 4);
+    for (int i = 0; i < level; i++) {
+      printf(" ");
+    }
+    if (node->data->is_dir) {
+      printf("%s/\n", node->data->name);
+    } else {
+      printf("%s\n", node->data->name);
+    }
   }
 }
-
-// Function to traverse the directory structure in post-order
-void traverse_postorder(struct file_node *node) {
-  if (node != NULL) {
-    println("Traversing in Postorder");
-    traverse_postorder(node->left_child);
-    traverse_postorder(node->right_child);
-    printf("%s\n", node->name);
+void print_bst_preorder(Node *node, int level) {
+  if (node) {
+    for (int i = 0; i < level; i++) {
+      printf(" ");
+    }
+    if (node->data->is_dir) {
+      printf("%s/\n", node->data->name);
+    } else {
+      printf("%s\n", node->data->name);
+    }
+    print_bst_preorder(node->right, level + 4);
+    print_bst_preorder(node->left, level + 4);
   }
 }
+// Delete a node from the binary search tree
+void delete_node(BST *bst, char *name) {
+  Node *parent = NULL;
+  Node *node = bst->root;
 
-// Function to print the directory structure in a formatted way
-void print_directory(struct file_node *node, int level) {
+  // Find the node to be deleted
+  while (node != NULL && strcmp(node->data->name, name) != 0) {
+    parent = node;
+    if (strcmp(name, node->data->name) < 0) {
+      node = node->left;
+    } else {
+      node = node->right;
+    }
+  }
+
+  // If the node was not found, return
   if (node == NULL) {
     return;
   }
-  print_directory(node->right_child, level + 1);
-  for (int i = 0; i < level; i++) {
-    printf(" ");
+
+  // If the node has no children, simply delete it
+  if (node->left == NULL && node->right == NULL) {
+    if (parent == NULL) {
+      bst->root = NULL;
+    } else if (node == parent->left) {
+      parent->left = NULL;
+    } else {
+      parent->right = NULL;
+    }
+    free(node->data);
+    free(node);
   }
-  if (node->is_directory) {
-    printf("Directory : %s/\n", node->name);
-  } else {
-    printf("File : %s\n", node->name);
+
+  // If the node has one child, replace it with its child
+  else if (node->left == NULL || node->right == NULL) {
+    Node *child = (node->left != NULL) ? node->left : node->right;
+    if (parent == NULL) {
+      bst->root = child;
+    } else if (node == parent->left) {
+      parent->left = child;
+    } else {
+      parent->right = child;
+    }
+    free(node->data);
+    free(node);
   }
-  print_directory(node->left_child, level + 1);
+
+  // If the node has two children, find the in-order predecessor (or successor)
+  // to replace it
+  else {
+    Node *predecessor = node->left;
+    parent = node;
+    while (predecessor->right != NULL) {
+      parent = predecessor;
+      predecessor = predecessor->right;
+    }
+    node->data = predecessor->data;
+    if (parent == node) {
+      parent->left = predecessor->left;
+    } else {
+      parent->right = predecessor->left;
+    }
+    free(predecessor->data);
+    free(predecessor);
+  }
 }
 
-// Function to parse user input and execute appropriate commands
 void parse_input(char *input) {
   char *token = strtok(input, " ");
   char *name;
   bool is_directory;
+  bool is_file;
   if (strcmp(token, "insert") == 0) {
     token = strtok(NULL, " ");
     is_directory = strcmp(token, "directory") == 0 ? true : false;
+    is_file = strcmp(token, "file") == 0 ? true : false;
     token = strtok(NULL, " ");
     name = token;
-    insert_node(name, is_directory);
+    NodeData node_data = {.name=name, .is_dir = is_directory};
+    node_data.is_dir = is_directory;
+    if (is_directory) {
+      node_data.is_dir = true;
+    }
+    if (!is_directory && !is_file) {
+      println("Not a valid insert type");
+      return;
+    }
+    insert(&bst, &node_data);
+    println("[ INFO ] : Successfully insertted");
   } else if (strcmp(token, "delete") == 0) {
     token = strtok(NULL, " ");
     name = token;
-    root = delete_node(root, name);
+    delete_node(&bst, name);
   } else if (strcmp(token, "search") == 0) {
     token = strtok(NULL, " ");
     name = token;
-    struct file_node *result = search_node(root, name);
+    Node *result = search_bst(bst.root, name);
     if (result != NULL) {
       printf("%s found\n", name);
     } else {
@@ -175,36 +223,45 @@ void parse_input(char *input) {
     }
   } else if (strcmp(token, "traverse") == 0) {
     token = strtok(NULL, " ");
-    println("token : %s", token);
     if (strcmp(token, "preorder") == 0) {
-        println("Inside preorder");
-        root == NULL? printf("NULL\n") : println("Not NULL");
-      traverse_preorder(root);
+      print_bst_preorder(bst.root, 0);
     } else if (strcmp(token, "inorder") == 0) {
-        println("Inside inorder");
-        root == NULL? printf("NULL\n") : println("Not NULL");
-      traverse_inorder(root);
+      print_bst_inorder(bst.root, 0);
     } else if (strcmp(token, "postorder") == 0) {
-        println("Inside postorder");
-        root == NULL? printf("NULL\n") : println("Not NULL");
-        root == NULL? printf("NULL\n") : println("Not NULL");
-      traverse_postorder(root);
+      print_bst_postorder(bst.root, 0);
     }
   } else if (strcmp(token, "print") == 0) {
-    print_directory(root, 0);
-  }else {
+    print_bst_preorder(bst.root, 0);
+  } else {
     println("Unknown command %s", token);
   }
 }
-
+// Example usage
 int main() {
+  //   NodeData root_dir_data = { .name = "root", .is_dir = 1 };
+  //   insert(&bst, &root_dir_data);
+  //   NodeData file1_data = { .name = "file1.txt", .is_dir = 0 };
+  //   insert(&bst, &file1_data);
+  //   NodeData dir1_data = { .name = "dir1", .is_dir = 1 };
+  //   insert(&bst, &dir1_data);
+  //   NodeData file2_data = { .name = "file2.txt", .is_dir = 0 };
+  //   insert(&bst, &file2_data);
+  //   NodeData file3_data = { .name = "file3.txt", .is_dir = 0 };
+  //   insert(&bst, &file3_data);
+  //   NodeData dir2_data = { .name = "dir2", .is_dir = 1 };
+  //   insert(&bst, &dir2_data);
+  //   NodeData file4_data = { .name = "file4.txt", .is_dir = 0 };
+  //   insert(&bst, &file4_data);
+  //   print_bst_inorder(bst.root, 0);
+  //   Node* result = search_bst(bst.root, "file4.");
   char input[100];
   while (true) {
     printf("Enter command: ");
-    fgets(input, sizeof(input), stdin);//used to take user input from keyboard
+    fgets(input, sizeof(input), stdin);
     if (strcmp(input, "exit\n") == 0) {
       break;
     }
+    input[strlen(input) - 1] = '\0';
     parse_input(input);
   }
   return 0;
